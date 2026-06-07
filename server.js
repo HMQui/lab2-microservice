a3a512ae4f504bd7bcc16b31d19104247f4f1e9e
HMQui <huynhqui6425@gmail.com>
2026-06-08 00:28:26 +0700

    Init Node.js microservice

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.json({
+        status: "Success",
+        message: "Hello from Lab 2 Microservice CI/CD!",
+        version: "1.0.0"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Microservice is running on port ${port}`);
+});
